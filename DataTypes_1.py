80563301abd1c8f598c1da13a6ea68ff62a02213
ShreyaRuhela <122210221+ShreyaRuhela@users.noreply.github.com>
2023-05-04 15:14:44 +0530

    Create DataTypes_1.py

---
+++ DataTypes_1.py
@@ -0,0 +1,36 @@
+#data type in pyhton
+
+#NUMERIC DATA TYPE
+#INTEGER 
+f=9
+print(type(f))
+#FLOAT
+g=5.999999999
+print(type(g))
+#COMPLEX
+h=complex(8, 5)
+print(type(h))
+
+#SEQUENCES DATA TYPE
+#string
+a="SHREYA"
+print(type(a))
+#list
+b=[1,3,4,"SHREYA",9 ]
+print(type(b))
+#tuples
+d=(1,5,7,"HERO")
+print(type(d))
+
+#DICTIONARIES
+c={1:"blue",2:"yellow"}
+print(type(c))
+
+#SET
+e={4,8,"hello"}
+print(type(e))
+
+#BOOLEAN
+print(8<10)
+
+
